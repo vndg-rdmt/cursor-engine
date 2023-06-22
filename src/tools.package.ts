a3c16f3d275fa153de72fa11d60865802a75a680
RDMTSTUDIOS <rdmtstudios@yandex.ru>
2023-06-23 01:08:53 +0300

    Added documentation, licence

--- src/tools.package.ts
+++ src/tools.package.ts
@@ -1,3 +1,9 @@
+/*------------------------------------------------------------------------------------------------
+ *  Copyright (c) RDMT STUDIOS 1986. All rights reserved.
+ *  Licensed under the Apache 2.0 License. See LICENSE in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+
 /**
  * Base abstract class for storing meta information
  * within package for tools and to also prevent naming collisions
